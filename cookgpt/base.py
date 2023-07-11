a4e892906c15432f252f73f38240ec216c46b714
mcsavvy <phishink5@gmail.com>
2023-07-11 17:10:27 +0100

    Renamed models.py -> base.py

--- cookgpt/base.py
+++ cookgpt/base.py
@@ -1,21 +1,46 @@
-from dynaconf import FlaskDynaconf
-from flask import Flask
+from datetime import datetime
+from uuid import uuid4
+
+from sqlalchemy_serializer import SerializerMixin
+
+from cookgpt.ext.database import db
 
 
-def create_app(**config):
-    app = Flask(__name__)
-    FlaskDynaconf(app)  # config managed by Dynaconf
-    app.config.load_extensions(
-        "EXTENSIONS"
-    )  # Load extensions from settings.toml
-    app.config.update(config)  # Override with passed config
-    return app
+class BaseModelMixin(SerializerMixin):
+    """Base model"""
 
+    id = db.Column(db.Uuid, primary_key=True, default=uuid4)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow
+    )
 
-def create_app_wsgi():
-    # workaround for Flask issue
-    # that doesn't allow **config
-    # to be passed to create_app
-    # https://github.com/pallets/flask/issues/4170
-    app = create_app()
-    return app
+    def __repr__(self):
+        return f"<{self.__class__.__name__} {self.id}>"
+
+    @classmethod
+    def create(cls, commit=True, **kwargs):
+        """Creates model"""
+        instance = cls(**kwargs)
+        if commit:
+            instance.save()
+        return instance
+
+    def update(self, commit=True, **kwargs):
+        """Updates model"""
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        if commit:
+            self.save()
+        return self
+
+    def save(self):
+        """Saves model"""
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self, commit=True):
+        """Deletes model"""
+        db.session.delete(self)
+        if commit:
+            db.session.commit()
