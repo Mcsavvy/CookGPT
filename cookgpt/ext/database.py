9bc4438a5d41b70714d6945da5f25a7d8c481684
mcsavvy <phishink5@gmail.com>
2023-07-20 23:57:33 +0100

    Added Migration & Customized Database Class

--- cookgpt/ext/database.py
+++ cookgpt/ext/database.py
@@ -1,7 +1,36 @@
+from typing import TYPE_CHECKING
+
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+if TYPE_CHECKING:
+    from sqlalchemy.orm.scoping import scoped_session
+
+
+class Database(SQLAlchemy):
+    """Database"""
+
+    session: "scoped_session"
+
+    def create_all(self, *args, **kwargs):
+        """Creates all"""
+        from cookgpt.auth.models import JwtToken  # noqa: F401
+        from cookgpt.user.models import User  # noqa: F401
+
+        super().create_all(*args, **kwargs)
+
+    def drop_all(self, *args, **kwargs):
+        """Drops all"""
+        from cookgpt.auth.models import JwtToken  # noqa: F401
+        from cookgpt.user.models import User  # noqa: F401
+
+        super().drop_all(*args, **kwargs)
+
+
+db = Database()
+migrate = Migrate()
 
 
 def init_app(app):
     db.init_app(app)
+    migrate.init_app(app, db)
