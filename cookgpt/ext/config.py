1744f0a178a098aea3673a8a49a6a34da14ad172
mcsavvy <phishink5@gmail.com>
2023-07-28 10:07:25 +0100

    Refactor config.py to use lowercase env variable

--- cookgpt/ext/config.py
+++ cookgpt/ext/config.py
@@ -6,8 +6,8 @@ from apiflask import APIFlask
 def configure_secret_key(app: "APIFlask"):
     if not app.config["SECRET_KEY"]:
         raise RuntimeError("SECRET_KEY not set.")
-    FLASK_ENV = app.config["ENV"]
-    if FLASK_ENV == "production":
+    env = app.config.current_env.lower()  # type: ignore
+    if env == "production":
         if not getenv("APP_SECRET_KEY"):
             raise RuntimeError("APP_SECRET_KEY not set.")
         app.config["SECRET_KEY"] = getenv("APP_SECRET_KEY")
@@ -16,8 +16,8 @@ def configure_secret_key(app: "APIFlask"):
 def configure_db_uri(app: "APIFlask"):
     if not app.config["SQLALCHEMY_DATABASE_URI"]:
         raise RuntimeError("SQLALCHEMY_DATABASE_URI not set.")
-    FLASK_ENV = app.config["ENV"]
-    if FLASK_ENV == "production":
+    env = app.config.current_env.lower()  # type: ignore
+    if env == "production":
         if not getenv("APP_DATABASE_URL"):
             raise RuntimeError("APP_DATABASE_URL not set.")
         app.config["SQLALCHEMY_DATABASE_URI"] = getenv("APP_DATABASE_URL")
