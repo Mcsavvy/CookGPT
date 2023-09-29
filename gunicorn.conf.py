94362a834ee30e7d4abd8bbfb7db01c70d29904a
mcsavvy <davemcsavvii@gmail.com>
2023-09-29 17:38:06 +0000

    chore: change gunicorn bind & server url

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,8 +3,8 @@ import os
 # Server Socket
 wsgi_app = os.environ["FLASK_APP"]
 reload = os.getenv("GUNICORN_RELOAD", "0") == "1"
-HOST = os.getenv("HOST", "localhost")
-PORT = os.getenv("PORT", "5000")
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = os.getenv("PORT", "8000")
 bind = os.getenv("GUNICORN_BIND", f"{HOST}:{PORT}")
 workers = int(os.getenv("GUNICORN_WORKERS", "1"))
 threads = int(os.getenv("GUNICORN_THREADS", "1"))
